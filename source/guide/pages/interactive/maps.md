66ae6bc5ab33a3a50d2e21823101f5c0c0bbc6d0
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-09-06 12:09:02 -0500

    Adding gl geojson.

--- source/guide/pages/interactive/maps.md
+++ source/guide/pages/interactive/maps.md
@@ -8,14 +8,35 @@ bottom: |
 
 Best practices and styles for using maps.
 
-## Mapbox example
+## Mapbox
+
+Mapbox is a great tool to make custom, design-specific maps.
+
+### Including the mapbox library
+
+You will need to include the Mapbox library. Since the maps will depend on the Mapbox service being up, it is fine to use the versions they provide through their site. Always make sure to use a specific version and not something like "latest". This should only need to be done once per page.
 
 {% example html %}
-<script src="https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js"></script>
-<link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.css" rel="stylesheet">
+
+<!-- Mapbox GL library -->
+<script src="https://api.tiles.mapbox.com/mapbox-gl-js/v0.48.0/mapbox-gl.js"></script>
+<link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.48.0/mapbox-gl.css" rel="stylesheet">
+
+<!-- Mapbox non-GL JS library -->
+<script src="https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js"></script>
+<link href="https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css" rel="stylesheet">
+{% endexample %}
+
+### Cross-platform maps
+
+The Strib styles maps module comes with some helpful functions for creating maps. For instance, if you would like to create a basic map that uses GL by default, but will fallback to the non-GL version if the browser doesn't support it, use the `MapboxCrossPlatformMap` class.
+
+Mapbox describes support for Mapbox GL as [all modern browsers](https://www.mapbox.com/help/mapbox-browser-support/) and specifically does not support Internet Explorer. [CanIUse](https://caniuse.com/#search=gl) has some good information about WebGL support.
+
+{% example html %}
 
 <figure>
-  <div id="map" class="figure-object figure-map"></div>
+  <div id="cross-map" class="figure-object figure-map"></div>
 
   <figcaption>
     This map depicts blah blah blah.  Some map and design data
@@ -25,27 +46,56 @@ Best practices and styles for using maps.
 </figure>
 
 <script>
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiODRHdjBSWSJ9.lF4ymp-69zdGvZ5X4Tokzg';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/shadowflare/cjfljh7oh6v6k2smuvb3jftac',
-    center: [-93.191872, 44.960911],
-    zoom: 9,
-    attributionControl: false
-  });
+  (function() {
+    // Create map with default options.
+    var map = new stribStyles.maps.MapboxCrossPlatformMap('cross-map', {});
 
-  var nav = new mapboxgl.NavigationControl();
-  map.addControl(nav, 'top-left');
+    // Show off some methods
+    var delay = 2500;
+    setTimeout(function() {
+      map.centerOn(stribStyles.maps.places.minneapolis);
+
+      setTimeout(function() {
+        map.zoomTo(8);
+
+        setTimeout(function() {
+          map.centerOn(stribStyles.maps.places.duluth, 7);
+
+          setTimeout(function() {
+            map.fitBounds(stribStyles.maps.bounds.minnesota);
+          }, delay);
+        }, delay);
+      }, delay);
+    }, delay);
+  })();
 </script>
 
 {% endexample %}
 
+Overall, this cross-platform map object is for simple maps. There is not a lot of shared functionality across the Mapbox libraries. There are also some key differences between the libraries and this custom object.
+
+- Assume options and standards for Mapbox GL, then translate to non-GL version.
+- Mapbox GL uses `[lng, lat]` format for points, and these should be used and will be translated to the non-GL version which is the opposite.
+
+If you want to work with the map and do specific things based on which platform is used, you can use the `gl` property. For example:
+
+{% highlight html %}
+
+<script>
+  var map = new stribStyles.maps.MapboxCrossPlatformMap('map', {});
+  if (map.gl) {
+    map.map.setPitch(30);
+  }
+</script>
+
+{% endhighlight %}
+
+#### Adding JSON
+
 {% example html %}
-<script src="https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js"></script>
-<link href="https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css" rel="stylesheet">
 
 <figure>
-  <div id="map-fallback" class="figure-object figure-map"></div>
+  <div id="cross-json-map" class="figure-object figure-map"></div>
 
   <figcaption>
     This map depicts blah blah blah.  Some map and design data
@@ -54,13 +104,22 @@ Best practices and styles for using maps.
   </figcaption>
 </figure>
 
+<script src="http://static.startribune.com/assets/libs/topojson/3.0.2/topojson.min.js"></script>
 <script>
-  L.mapbox.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiODRHdjBSWSJ9.lF4ymp-69zdGvZ5X4Tokzg';
-  var fallbackMap = L.mapbox.map('map-fallback', undefined, {
-    attributionControl: false,
-  }).setView([44.960911, -93.191872], 9 + 1);
-  var styleLayer = L.mapbox.styleLayer('mapbox://styles/shadowflare/cjfljh7oh6v6k2smuvb3jftac')
-      .addTo(fallbackMap);
+  (function() {
+    // Create map with default options.
+    var map = new stribStyles.maps.MapboxCrossPlatformMap('cross-json-map', {});
+    map.fitBounds(stribStyles.maps.bounds.minnesota);
+
+    // Get some sample data
+    window.fetch('https://raw.githubusercontent.com/deldersveld/topojson/master/countries/us-states/MN-27-minnesota-counties.json').then(response => response.json()).then(counties => {
+      console.log(topojson.feature(counties, counties.objects.cb_2015_minnesota_county_20m));
+      map.addGeoJSON(
+        'counties',
+        topojson.feature(counties, counties.objects.cb_2015_minnesota_county_20m),
+        {});
+    });
+  })();
 </script>
 
 {% endexample %}
--- source/js/maps.js
+++ source/js/maps.js
@@ -4,6 +4,8 @@
 
 // Dependencies or globals
 /* global mapboxgl, L */
+import { defaultsDeep, cloneDeep } from 'lodash-es';
+import stribStyles from './styles.js';
 
 // Access token.  This is a public key, but this should not be used
 // outside of Star Tribune
@@ -15,9 +17,18 @@ const mapboxStyles = {
   light: 'mapbox://styles/shadowflare/cjfljh7oh6v6k2smuvb3jftac'
 };
 
-// Common places
+// Common places in lng, lat
 const places = {
-  twinCities: [-93.191872, 44.960911]
+  twinCities: [-93.191872, 44.960911],
+  minneapolis: [-93.265, 44.9778],
+  stPaul: [-93.09, 44.9537],
+  duluth: [-92.1005, 46.7867],
+  minnesota: [-94.6859, 46.7296]
+};
+
+// Common bounds in [sw, ne]
+const bounds = {
+  minnesota: [[-97.37, 43.42], [-89.44, 49.46]]
 };
 
 // Mapbox attribution
@@ -40,18 +51,15 @@ function mapboxGLMap(el, options = {}) {
   }
 
   // Default options
-  let mapOptions = Object.assign(
-    {
-      container: el,
-      style: mapboxStyles.light,
-      center: places.twinCities,
-      zoom: 9,
-      attributionControl: false,
-      navigationControl: true,
-      scrollZoom: false
-    },
-    options
-  );
+  let mapOptions = defaultsDeep(options, {
+    container: el,
+    style: mapboxStyles.light,
+    center: places.twinCities,
+    zoom: 9,
+    attributionControl: false,
+    navigationControl: true,
+    scrollZoom: false
+  });
 
   // Set access token
   mapboxgl.accessToken = mapboxAccessToken;
@@ -87,22 +95,17 @@ function mapboxNoGLMap(el, options = {}) {
   L.mapbox.accessToken = mapboxAccessToken;
 
   // Default options
-  let mapOptions = Object.assign(
-    {
-      style: mapboxStyles.light,
-      center: places.twinCities,
-      zoom: 10,
-      attributionControl: false,
-      navigationControl: true,
-      scrollWheelZoom: false
-    },
-    options
-  );
+  let mapOptions = defaultsDeep(options, {
+    style: mapboxStyles.light,
+    center: places.twinCities,
+    zoom: 10,
+    attributionControl: false,
+    navigationControl: true,
+    scrollWheelZoom: false
+  });
 
   // Create map
-  let map = L.mapbox.map(el, undefined, {
-    attributionControl: false
-  });
+  let map = L.mapbox.map(el, undefined, mapOptions);
 
   // Add style layer
   if (mapOptions.style) {
@@ -112,12 +115,186 @@ function mapboxNoGLMap(el, options = {}) {
   return map;
 }
 
+// A class to make a "cross-platform" map with some basic shared
+// functionality
+class MapboxCrossPlatformMap {
+  constructor(el, options = {}, forceGL = undefined) {
+    this.el = el;
+
+    // Overall, we use GL options and translate to noGL
+    this.mapOptions = defaultsDeep(
+      {
+        style: mapboxStyles.light,
+        center: places.twinCities,
+        zoom: 9,
+        attributionControl: false,
+        navigationControl: true,
+        scrollZoom: false
+      },
+      options
+    );
+
+    this.checkPlatform(forceGL);
+    this.createMap();
+  }
+
+  // Check options and browser
+  checkPlatform(forceGL = undefined) {
+    // Check for mapbox libraries
+    if (!mapboxgl || !window.mapboxgl) {
+      throw new Error(
+        'Please provide an element to attach the map to, this should be the "id" attribute of the element.'
+      );
+    }
+    if (!L || !window.L || !L.mapbox) {
+      throw new Error(
+        'Could not find "L" Leaflet object or the "L.mapbox" object, make sure you have loaded the library.  See https://www.mapbox.com/mapbox.js/'
+      );
+    }
+
+    // Make sure there is an el
+    if (!this.el) {
+      throw new Error(
+        'Please provide an element to attach the map to, this should be the "id" attribute of the element.'
+      );
+    }
+
+    // Get GL support
+    this.gl = forceGL === undefined ? mapboxgl.supported() : !!forceGL;
+  }
+
+  // Create map
+  createMap() {
+    if (this.gl) {
+      this.map = mapboxGLMap(this.el, this.mapOptionsGL(this.mapOptions));
+    }
+    else {
+      this.map = mapboxNoGLMap(this.el, this.mapOptionsNoGL(this.mapOptions));
+    }
+  }
+
+  // Translate options for GL
+  mapOptionsGL(options = {}) {
+    return cloneDeep(options);
+  }
+
+  // Translate options for noGL
+  mapOptionsNoGL(options = {}) {
+    let o = cloneDeep(options);
+
+    // Leaflet uses lat, lng
+    o.center =
+      o.center && o.center.length
+        ? o.center.reverse()
+        : places.twinCities.reverse();
+    o.zoom = o.zoom ? o.zoom + 1 : 10;
+    o.scrollWheelZoom = o.scrollZoom;
+
+    return o;
+  }
+
+  // Set view (center and zoom)
+  goTo(center, zoom) {
+    if (this.gl) {
+      this.map.easeTo({ center, zoom });
+    }
+    else {
+      this.map.setView(center.reverse(), zoom + 1);
+    }
+  }
+
+  // Zoom to
+  zoomTo(zoom) {
+    if (this.gl) {
+      this.map.easeTo({ zoom });
+    }
+    else {
+      this.map.setZoom(zoom + 1);
+    }
+  }
+
+  // Center on
+  centerOn(center) {
+    if (this.gl) {
+      this.map.easeTo({ center });
+    }
+    else {
+      this.map.setView(center.reverse());
+    }
+  }
+
+  // Fit bounds.  Should be an array, like  [sw, ne] with [lng, lat]s
+  // https://www.mapbox.com/mapbox-gl-js/api/#lnglatboundslike
+  fitBounds(bounds) {
+    if (this.gl) {
+      this.map.fitBounds(bounds);
+    }
+    else {
+      this.map.fitBounds(bounds.map(b => b.reverse()));
+    }
+  }
+
+  // Add JSON
+  addGeoJSON(id, geojson, styles) {
+    if (this.gl) {
+      return this.addGeoJSONGL(id, geojson, styles);
+    }
+  }
+
+  // Add GeoJSON GL
+  addGeoJSONGL(id, geojson, styles) {
+    styles = defaultsDeep(styles, {
+      // https://www.mapbox.com/mapbox-gl-js/style-spec/#layers-fill
+      'fill-color': stribStyles['colors-data'].categories[0].hex,
+      'fill-opacity': 0.8,
+      'fill-outline-color': '#FFFFFF'
+    });
+
+    // Put geojson under the label
+    // https://www.mapbox.com/mapbox-gl-js/example/geojson-layer-in-stack/
+    this.map.on('load', () => {
+      let layers = this.map.getStyle().layers;
+
+      // Find the index of the first symbol layer in the map style
+      let firstSymbolId;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+          firstSymbolId = layers[i].id;
+          break;
+        }
+      }
+
+      // Create data source layer
+      this.dataSource = {};
+      this.dataSource[id] = this.map.addSource(id, {
+        type: 'geojson',
+        data: geojson
+      });
+
+      // Add style layer
+      this.jsonLayers = {};
+      this.jsonLayers[id] = this.map.addLayer(
+        {
+          id: `${id}-json`,
+          type: 'fill',
+          source: id,
+          paint: styles
+          //"filter": ["==", "$type", "Polygon"]
+        },
+        firstSymbolId
+      );
+    });
+  }
+}
+
 // Export
 export default {
   mapboxAccessToken,
   mapboxStyles,
   mapboxGLMap,
   mapboxNoGLMap,
+  MapboxCrossPlatformMap,
   mapboxAttribution,
-  places
+  places,
+  bounds
 };
